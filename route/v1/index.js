2b94e7aca43ca41a7bb7c26b8e3ede752f6602ee
vimalk179 <vimalk179@gmail.com>
2024-05-13 18:12:23 +0530

    COde updated

--- route/v1/index.js
+++ route/v1/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const aws_route = require('./aws_route');
-
 const bucket_route = require('./bucket_route')
 const object_route = require('./object_route')
 // const routes = require('./routes')
@@ -13,10 +11,7 @@ const defaultRoutes = [
         path: '/object',
         route: object_route,
     },
-    {
-        path: '/aws_route',
-        route: aws_route,
-    },
+
     {
         path: '/bucket',
         route: bucket_route,
